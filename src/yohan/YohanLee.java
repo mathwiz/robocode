8800d85eb787a99a48472a2a49d25184acd71b60
mathwiz <mathwiz@gmail.com>
2014-08-17 16:58:19 -0400

    Personalities

--- src/yohan/YohanLee.java
+++ src/yohan/YohanLee.java
@@ -13,33 +13,26 @@ public class YohanLee extends AdvancedRobot {
 
     public void run() {
         setBodyColor(Color.WHITE);
+        setPersonality();
 
+        while (true) {
+            r.execute();
+        }
+    }
+
+    private void setPersonality() {
         if (new Date().getTime() % 2 == 0) {
             r = new RamPersonality(this);
         } else {
             r = new WallsPersonality(this);
         }
         r.init();
-        while (true) {
-            r.execute();
-        }
     }
 
-    private void changePersonality() {
-        r = new RamPersonality(this);
-        r.init();
-    }
-
-    /**
-     * onHitRobot:  Move away a bit.
-     */
     public void onHitRobot(HitRobotEvent e) {
         r.onHitRobot(e);
     }
 
-    /**
-     * onScannedRobot:  Fire!
-     */
     public void onScannedRobot(ScannedRobotEvent e) {
         r.onScannedRobot(e);
     }
