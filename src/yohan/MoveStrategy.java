acd7cba1ee12961e59d3dbd6b912ac1d1bb28e03
mathwiz <mathwiz@gmail.com>
2014-08-17 13:25:00 -0400

    Initial

---
+++ src/yohan/MoveStrategy.java
@@ -0,0 +1,8 @@
+package yohan;
+
+/**
+ * Created by Yohan on 8/17/2014.
+ */
+public interface MoveStrategy {
+    void move();
+}
---
+++ src/yohan/YohanLee.java
@@ -0,0 +1,77 @@
+package yohan;
+
+import robocode.*;
+import robocode.Robot;
+
+import java.awt.*;
+
+public class YohanLee extends AdvancedRobot {
+    private static int scans;
+    private MoveStrategy moveStrategy;
+
+    public YohanLee() {
+        final Robot me = this;
+        moveStrategy = new MoveStrategy() {
+            @Override
+            public void move() {
+            }
+        };
+    }
+
+    public MoveStrategy getMoveStrategy() {
+        return moveStrategy;
+    }
+
+    public void setMoveStrategy(MoveStrategy moveStrategy) {
+        this.moveStrategy = moveStrategy;
+    }
+
+    /**
+     * run: YohanRobot's default behavior
+     */
+    public void run() {
+        // Initialization of the robot should be put here
+        setColors(Color.lightGray, Color.darkGray, Color.black); // body,gun,radar
+
+        // Robot main loop
+        while (true) {
+            // Tell the game that when we take move,
+            // we'll also want to turn right... a lot.
+            setTurnRight(10000);
+            // Limit our speed to 5
+            setMaxVelocity(5);
+            // Start moving (and turning)
+            ahead(10000);
+            // Repeat.
+        }
+    }
+
+    /**
+     * onScannedRobot: What to do when you see another robot
+     */
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        // Replace the next line with any behavior you would like
+        scans++;
+        if (scans % 2 == 0)
+            fire(3);
+    }
+
+    /**
+     * onHitByBullet: What to do when you're hit by a bullet
+     */
+    public void onHitByBullet(HitByBulletEvent e) {
+        // Replace the next line with any behavior you would like
+        turnRight(90);
+        back(10);
+    }
+
+    /**
+     * onHitWall: What to do when you hit a wall
+     */
+    public void onHitWall(HitWallEvent e) {
+        // Replace the next line with any behavior you would like
+        back(100);
+        turnRight(90);
+    }
+}
