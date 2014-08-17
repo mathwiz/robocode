2ecce6043fec332ac213f67b8cd761447f58d5e4
mathwiz <mathwiz@gmail.com>
2014-08-17 15:04:26 -0400

    some more

--- src/yohan/SuperAdvancedRobot.java
+++ src/yohan/SuperAdvancedRobot.java
@@ -1,12 +1,22 @@
 package yohan;
 
 import robocode.AdvancedRobot;
+import robocode.Robot;
 
 /**
  * Created by Yohan on 8/17/2014.
  */
 public abstract class SuperAdvancedRobot extends AdvancedRobot {
     private MoveStrategy moveStrategy;
+    private Robot current;
+
+    public Robot getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(Robot current) {
+        this.current = current;
+    }
 
     public MoveStrategy getMoveStrategy() {
         return moveStrategy;
--- src/yohan/YohanLee.java
+++ src/yohan/YohanLee.java
@@ -1,13 +1,35 @@
 package yohan;
 
 import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
 import robocode.HitWallEvent;
 import robocode.ScannedRobotEvent;
 
 import java.awt.*;
+import java.util.Random;
 
 public class YohanLee extends SuperAdvancedRobot {
     private static int scans;
+    private MoveStrategy[] moveStrategies = new MoveStrategy[2];
+    private int turnDirection = 1;
+    private Random random = new Random();
+
+    public YohanLee() {
+        moveStrategies[0] = new MoveStrategy() {
+            @Override
+            public void move() {
+                setTurnRight(10000);
+                setMaxVelocity(5);
+                ahead(10000);
+            }
+        };
+        moveStrategies[1] = new MoveStrategy() {
+            @Override
+            public void move() {
+                turnRight(5 * turnDirection);
+            }
+        };
+    }
 
     /**
      * run: YohanRobot's default behavior
@@ -15,16 +37,10 @@ public class YohanLee extends SuperAdvancedRobot {
     public void run() {
         // Initialization of the robot should be put here
         setColors(Color.BLACK, Color.ORANGE, Color.GRAY); // body,gun,radar
-        setMoveStrategy(new MoveStrategy() {
-            @Override
-            public void move() {
-                setTurnRight(10000);
-                setMaxVelocity(5);
-                ahead(10000);
-            }
-        });
+        getCurrent().run();
+        setMoveStrategy(moveStrategies[1]);
 
-        // Robot main loop
+//        // Robot main loop
         while (true) {
             getMoveStrategy().move();
         }
@@ -35,27 +51,52 @@ public class YohanLee extends SuperAdvancedRobot {
      */
 
     public void onScannedRobot(ScannedRobotEvent e) {
-        // Replace the next line with any behavior you would like
-        scans++;
-        if (scans % 2 == 0)
-            fire(3);
+        setMoveStrategy(moveStrategies[1]);
+        if (e.getBearing() >= 0) {
+            turnDirection = 1;
+        } else {
+            turnDirection = -1;
+        }
+
+        turnRight(e.getBearing());
+        ahead(e.getDistance() + 5);
+        scan(); // Might want to move ahead again!
     }
 
     /**
      * onHitByBullet: What to do when you're hit by a bullet
      */
     public void onHitByBullet(HitByBulletEvent e) {
-        // Replace the next line with any behavior you would like
-        turnRight(90);
-        back(10);
+    }
+
+    public void onHitRobot(HitRobotEvent e) {
+        setMoveStrategy(moveStrategies[1]);
+        if (e.getBearing() >= 0) {
+            turnDirection = 1;
+        } else {
+            turnDirection = -1;
+        }
+        turnRight(e.getBearing());
+
+        // Determine a shot that won't kill the robot...
+        // We want to ram him instead for bonus points
+        if (e.getEnergy() > 16) {
+            fire(3);
+        } else if (e.getEnergy() > 10) {
+            fire(2);
+        } else if (e.getEnergy() > 4) {
+            fire(1);
+        } else if (e.getEnergy() > 2) {
+            fire(.5);
+        } else if (e.getEnergy() > .4) {
+            fire(.1);
+        }
+        ahead(40); // Ram him again!
     }
 
     /**
      * onHitWall: What to do when you hit a wall
      */
     public void onHitWall(HitWallEvent e) {
-        // Replace the next line with any behavior you would like
-        back(100);
-        turnRight(90);
     }
 }
