855ec9962c086a9babdba9daf997f488ee2beede
mathwiz <mathwiz@gmail.com>
2014-09-20 11:03:11 -0400

    Personalities

--- src/Spring/Team3/YohanLee.java
+++ src/Spring/Team3/YohanLee.java
@@ -1,9 +1,7 @@
 package Spring.Team3;
 
-import robocode.AdvancedRobot;
-import robocode.HitRobotEvent;
+import robocode.*;
 import robocode.Robot;
-import robocode.ScannedRobotEvent;
 
 import java.awt.*;
 
@@ -13,13 +11,12 @@ import java.awt.*;
 public class YohanLee extends AdvancedRobot {
     Personality p;
 
-
     private Personality getRobot() {
         return p;
     }
 
     public void run() {
-        setBodyColor(Color.MAGENTA);
+        setBodyColor(Color.CYAN);
         setGunColor(Color.WHITE);
         setRadarColor(Color.WHITE);
         p = new WallsPersonality(this);
@@ -27,5 +24,60 @@ public class YohanLee extends AdvancedRobot {
         while (true) {
             getRobot().execute();
         }
+    }
+
+    @Override
+    public void onStatus(StatusEvent statusEvent) {
+        getRobot().onStatus(statusEvent);
+    }
+
+    @Override
+    public void onBulletHit(BulletHitEvent bulletHitEvent) {
+        getRobot().onBulletHit(bulletHitEvent);
+    }
+
+    @Override
+    public void onBulletHitBullet(BulletHitBulletEvent bulletHitBulletEvent) {
+        getRobot().onBulletHitBullet(bulletHitBulletEvent);
+    }
+
+    @Override
+    public void onBulletMissed(BulletMissedEvent bulletMissedEvent) {
+        getRobot().onBulletMissed(bulletMissedEvent);
+    }
+
+    @Override
+    public void onDeath(DeathEvent deathEvent) {
+        getRobot().onDeath(deathEvent);
+    }
+
+    @Override
+    public void onHitByBullet(HitByBulletEvent hitByBulletEvent) {
+        getRobot().onHitByBullet(hitByBulletEvent);
+    }
+
+    @Override
+    public void onHitRobot(HitRobotEvent e) {
+        getRobot().onHitRobot(e);
+    }
+
+    @Override
+    public void onHitWall(HitWallEvent hitWallEvent) {
+        getRobot().onHitWall(hitWallEvent);
+    }
+
+    @Override
+    public void onScannedRobot(ScannedRobotEvent e) {
+        getRobot().onScannedRobot(e);
+    }
+
+    @Override
+    public void onRobotDeath(RobotDeathEvent robotDeathEvent) {
+        getRobot().onRobotDeath(robotDeathEvent);
+    }
+
+    @Override
+    public void onWin(WinEvent winEvent) {
+        getRobot().onWin(winEvent);
     }
 }
