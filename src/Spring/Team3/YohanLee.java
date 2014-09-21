e31f3ddaf3b5ffe6a36cb8dfd82801ef9b280bf2
mathwiz <mathwiz@gmail.com>
2014-09-20 21:24:34 -0400

    Ram

--- src/Spring/Team3/YohanLee.java
+++ src/Spring/Team3/YohanLee.java
@@ -73,7 +73,7 @@ public class YohanLee extends AdvancedRobot {
     }
 
     public void run() {
-//        setAdjustGunForRobotTurn(true);
+        //        setAdjustGunForRobotTurn(true);
         while (true) {
             turnRight(5 * getTurnDirection());
         }
@@ -94,10 +94,10 @@ public class YohanLee extends AdvancedRobot {
     @Override
     public void onHitByBullet(HitByBulletEvent e) {
         log("bullet hit from %s", e.getName());
-//        turnRight(getTurnSize());
-//        ahead(getEscapeDistance());
-//        flipEscapeDistance();
-//        scan();
+        //        turnRight(getTurnSize());
+        //        ahead(getEscapeDistance());
+        //        flipEscapeDistance();
+        //        scan();
     }
 
     @Override
@@ -115,9 +115,6 @@ public class YohanLee extends AdvancedRobot {
             turnRight(bearing);
             ahead(e.getDistance() + 5);
             scan();
-        } else {
-//            turnGunRight(getHeading() - getGunHeading() + e.getBearing());
-            fire(1);
         }
     }
 
@@ -128,7 +125,7 @@ public class YohanLee extends AdvancedRobot {
 
     @Override
     public void onStatus(StatusEvent e) {
-//        setTurnRadarRightRadians(Double.POSITIVE_INFINITY);
+        //        setTurnRadarRightRadians(Double.POSITIVE_INFINITY);
     }
 
     @Override
@@ -144,8 +141,8 @@ public class YohanLee extends AdvancedRobot {
     @Override
     public void onBulletHitBullet(BulletHitBulletEvent e) {
         log("bullet hit bullet. wtf?");
-        //        turnRight(getTurnDirection() * getTurnSize());
-        //        ahead(escapeDistance);
+        turnRight(getTurnDirection() * getTurnSize());
+        ahead(getEscapeDistance());
     }
 
     @Override
